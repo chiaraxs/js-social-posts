c9e7fdb6f6525f507021b54993b35c17e7bdb52d
chiaraxs <chiaracardinale92@gmail.com>
2022-01-11 14:03:54 +0100

    first setup

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Milestone 1
+// Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// id del post,
+// numero progressivo da 1 a n nome autore,
+// foto autore,
+// data in formato americano (mm-gg-yyyy),
+// testo del post,
+// immagine (non tutti i post devono avere una immagine),
+// numero di likes.
+
+// Milestone 2
+// Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+
+// Milestone 3
+// Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
